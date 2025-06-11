a14f5aecd6f049d97aae05194dd88f358a0f6830
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2025-06-11 19:14:19 -0400

    gyroscope implementation via DeviceRotation / DeviceRotationEvent

--- flixel/FlxG.hx
+++ flixel/FlxG.hx
@@ -47,6 +47,9 @@ import flixel.input.android.FlxAndroidKeys;
 #if FLX_ACCELEROMETER
 import flixel.input.FlxAccelerometer;
 #end
+#if FLX_GYROSCOPE
+import flixel.input.FlxGyroscope;
+#end
 #if FLX_POINTER_INPUT
 import flixel.input.FlxSwipe;
 #end
@@ -280,6 +283,13 @@ class FlxG
 	 * Provides access to the accelerometer data of mobile devices as `x`/`y`/`z` values.
 	 */
 	public static var accelerometer(default, null):FlxAccelerometer;
+	#end
+
+	#if FLX_GYROSCOPE
+	/**
+	 * Provides access to the accelerometer data of mobile devices as `pitch`/`roll`/`yaw` values.
+	 */
+	public static var gyroscope(default, null):FlxGyroscope;
 	#end
 
 	#if js
@@ -594,6 +604,10 @@ class FlxG
 
 		#if FLX_ACCELEROMETER
 		accelerometer = new FlxAccelerometer();
+		#end
+
+		#if FLX_GYROSCOPE
+		gyroscope = new FlxGyroscope();
 		#end
 
 		#if FLX_SAVE
