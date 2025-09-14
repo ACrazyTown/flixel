f6450e5103e80ba96dc558c344cb992c276b7f64
George Kurelic <Gkurelic@gmail.com>
2025-09-14 10:43:41 -0700

    update uv in clip() (#3476)

--- flixel/graphics/frames/FlxFrame.hx
+++ flixel/graphics/frames/FlxFrame.hx
@@ -665,6 +665,8 @@ class FlxFrame implements IFlxDestroyable
 			cacheFrameMatrix();
 		}
 		
+		updateUV();
+		
 		frameRect.put();
 		return this;
 	}
@@ -719,15 +721,21 @@ class FlxFrame implements IFlxDestroyable
 
 	function set_frame(value:FlxRect):FlxRect
 	{
-		if (value != null)
-		{
-			if (uv == null)
-				uv = FlxUVRect.get();
+		frame = value;
+		updateUV();
+		
+		return value;
+	}
+	
+	function updateUV()
+	{
+		if (frame == null)
+			return;
+		
+		if (uv == null)
+			uv = FlxUVRect.get();
 
-			uv.set(value.x / parent.width, value.y / parent.height, value.right / parent.width, value.bottom / parent.height);
-		}
-
-		return frame = value;
+		uv.setFromFrameRect(frame, parent);
 	}
 }
 
@@ -779,6 +787,11 @@ abstract FlxUVRect(FlxRect) from FlxRect to flixel.util.FlxPool.IFlxPooled
 	public inline function set(l, t, r, b)
 	{
 		this.set(l, t, r, b);
+	}
+	
+	public inline function setFromFrameRect(frame:FlxRect, parent:FlxGraphic)
+	{
+		this.set(frame.x / parent.width, frame.y / parent.height, frame.right / parent.width, frame.bottom / parent.height);
 	}
 	
 	public inline function copyTo(uv:FlxUVRect)
