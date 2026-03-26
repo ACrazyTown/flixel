5f372853af4acf18ebd095e7af3a21258ac86ec4
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2026-03-26 17:51:55 +0100

    some fixes

--- flixel/system/render/quad/FlxDrawTrianglesItem.hx
+++ flixel/system/render/quad/FlxDrawTrianglesItem.hx
@@ -59,7 +59,7 @@ class FlxDrawTrianglesItem extends FlxDrawBaseItem<FlxDrawTrianglesItem>
 		
 		#if !flash
 		var shader = shader != null ? shader : graphics.shader;
-		shader.bitmap.input = graphics.bitmap;
+		shader.bitmap.input = graphics.texture.handle;
 		shader.bitmap.filter = (camera.view.antialiasing || antialiasing) ? LINEAR : NEAREST;
 		shader.bitmap.wrap = REPEAT; // in order to prevent breaking tiling behaviour in classes that use drawTriangles
 		shader.alpha.value = alphas;
