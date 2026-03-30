8a06d8e9893ba0cba8b8a031f42d603b56fd7ac6
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2026-03-30 15:53:54 +0200

    fix invalid target in `framebufferRenderbuffer()`

--- flixel/system/render/gl/FlxGLRenderer.hx
+++ flixel/system/render/gl/FlxGLRenderer.hx
@@ -275,7 +275,7 @@ class FlxGLRenderer extends FlxTypedRenderer<FlxGLView>
             GL.renderbufferStorage(GL.RENDERBUFFER, GL.DEPTH24_STENCIL8, texture.width, texture.height);
 
             // Attach it to the framebuffer
-            GL.framebufferRenderbuffer(GL.RENDERBUFFER, GL.DEPTH_STENCIL_ATTACHMENT, GL.RENDERBUFFER, target.renderbuffer);
+            GL.framebufferRenderbuffer(GL.FRAMEBUFFER, GL.DEPTH_STENCIL_ATTACHMENT, GL.RENDERBUFFER, target.renderbuffer);
         }
     }
 }
