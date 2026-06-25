e19228017c8930dd0d8901822dd7bdc4f57f5f3e
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2026-06-25 19:45:37 +0200

    move typedefs to FlxRendererTypes

--- flixel/system/render/FlxRendererTypes.hx
+++ flixel/system/render/FlxRendererTypes.hx
@@ -11,3 +11,22 @@ typedef FlxTextureHandle = #if FLX_RENDER_OPENGL lime.graphics.opengl.GLTexture 
  * The actual type is dependant on the renderer, and is determined at compile time.
  */
 typedef FlxRenderTargetHandle = #if FLX_RENDER_OPENGL flixel.system.render.gl.FlxGLRenderTarget #else Dynamic #end;
+
+/**
+ * Underlying representation of a shader program.
+ * The actual type is dependant on the renderer, and is determined at compile time.
+ */
+typedef FlxShaderHandle = #if FLX_RENDER_OPENGL lime.graphics.opengl.GLProgram #else Dynamic #end;
+
+/**
+ * Underlying representation of a shader attribute location.
+ * The actual type is dependant on the renderer, and is determined at compile time.
+ */
+typedef FlxShaderAttributeLocation = Int;
+
+/**
+ * Underlying representation of a shader uniform location.
+ * The actual type is dependant on the renderer, and is determined at compile time.
+ */
+typedef FlxShaderUniformLocation = #if FLX_RENDER_OPENGL lime.graphics.opengl.GLUniformLocation #else Dynamic #end;
+
